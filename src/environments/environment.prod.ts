d778063ba6123cb73a15570cc6036fab7561d41a
szymonhelmecki <szhelmecki@gmail.com>
2026-07-07 10:38:33 +0200

    Save settings

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,8 +2,8 @@ export const environment = {
   production: true,
   apiUrl: 'https://mail-sender-backend-beeud5f5h5fncta0.polandcentral-01.azurewebsites.net',
   auth0: {
-    domain: 'flatrenty-dev.eu.auth0.com',
-    clientId: 'IvjGuxb21BBj1bHZKsX1uBTzqiQkNnhl',
-    audience: 'https://localhost:3000',
+    domain: 'flatrenty.eu.auth0.com',
+    clientId: 'VoJy3DRb6xH1lLcEQLERgJPHvuWDEzkJ',
+    audience: 'https://szymonhel.github.io/ng-mail-builder',
   },
 };
